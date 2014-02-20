5d28f316ae781f731a4ae8821225bb1806835446
chao2 <380692952@qq.com>
2014-02-20 13:37:38 +0800

    missing 'this' when generate constructor

--- inherit.js
+++ inherit.js
@@ -6,17 +6,19 @@
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl.html
  * 
- * source: https://raw.github.com/dfilatov/jquery-plugins/master/src/jquery.inherit/jquery.inherit.js
- *		   https://github.com/dfilatov/jquery-plugins/tree/master/src/jquery.inherit
+ * @source: https://raw.github.com/dfilatov/jquery-plugins/master/src/jquery.inherit/jquery.inherit.js
+ *          https://github.com/dfilatov/jquery-plugins/tree/master/src/jquery.inherit
  *
- * @modified   chao2(i@peichao01.com)
+ * @modified   peic@ctrip.com
  * @modify     delete the compatible codes, only for the mordern mobile browsers
+ *
+ * @git        https://github.com/peichao01/inherit.js/blob/master/inherit.js
  *
  * @dependencies: none
  *
  * @version 1.3.6
  */
-define("inherit", [], function(require,exports,module) {
+define([], function() {
     var parentName = '__base', constructorName = '__constructor', ClassName = '__self',
         eachObj = function (list, iterator) { for (var key in list) if (list.hasOwnProperty(key)) iterator(key, list[key]) },
 	    isFunction = function(fn){return Object.prototype.toString.call(fn)==='[object Function]'},
@@ -44,14 +46,14 @@ define("inherit", [], function(require,exports,module) {
     * @param {String}   [name]    -- ClassName
     * @param {Object}   [statics] -- static member of Class
     */
-    module.exports = function inherit(options) {
-        if(!options.proto) throw new Error("[inherit] no proto property was set.");
+    return window.inherit = function inherit(options) {
         var base = options.base || emptyBase,
-            result = (options.proto[constructorName] || (options.base && base.prototype[constructorName]))
-                    ? new Function("return function " + (options.name || "") + "(){this." + constructorName + ".apply(arguments)}")()
+            proto = options.proto || {},
+            result = (proto[constructorName] || (options.base && base.prototype[constructorName]))
+                    ? new Function("return function " + (options.name || "") + "(){this." + constructorName + ".apply(this, arguments)}")()
                     : function () { };
         if(!options.base) {
-            result.prototype = options.proto;
+            result.prototype = proto;
             result.prototype[ClassName] = result.prototype.constructor = result;
             return extend(result, options.statics);
         }
@@ -59,7 +61,7 @@ define("inherit", [], function(require,exports,module) {
         resultPtp[ClassName] = resultPtp.constructor = result;
 
         extend(result, base);//copy the static member from base to result
-        override(basePtp, resultPtp, options.proto);//override prototype member
+        override(basePtp, resultPtp, proto);//override prototype member
         options.statics && override(base, result, options.statics);//override static member
 
         return result;
